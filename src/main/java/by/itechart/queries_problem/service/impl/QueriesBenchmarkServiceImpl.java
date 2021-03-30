a00c9832f4643d02482a9f20942cff44205e6adc
azelikau <andrey.zelikay@gmail.com>
2021-03-30 15:01:12 +0300

    measure time from java code

--- src/main/java/by/itechart/queries_problem/service/impl/QueriesBenchmarkServiceImpl.java
+++ src/main/java/by/itechart/queries_problem/service/impl/QueriesBenchmarkServiceImpl.java
@@ -1,34 +1,78 @@
 package by.itechart.queries_problem.service.impl;
 
-import by.itechart.queries_problem.service.model.BenchmarkResult;
-import by.itechart.queries_problem.db_queries_benchmark.benchmark_executor.BenchmarkExecutorProvider;
+import by.itechart.queries_problem.db_queries_benchmark.ConnectionsHolder;
+import by.itechart.queries_problem.exception.GlobalExceptionHandler;
+import by.itechart.queries_problem.exception.SQLExceptionWrapper;
 import by.itechart.queries_problem.service.QueriesBenchmarkService;
+import by.itechart.queries_problem.service.model.BenchmarkResult;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 @RequiredArgsConstructor
 @Service
 public class QueriesBenchmarkServiceImpl implements QueriesBenchmarkService {
 
-    private final BenchmarkExecutorProvider benchmarkExecutorProvider;
+    private final ConnectionsHolder connectionsHolder;
 
-    private final int defaultNumberOfRepetitions = 10;
+    private final static int defaultNumberOfRepetitions = 10;
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @Override
     public Map<String, BenchmarkResult> executeBenchmarks(String query, Integer numberOfRepetitions) {
-        return benchmarkExecutorProvider.getExecutorMap().entrySet()
+        return connectionsHolder.getDbConnectionMap().entrySet()
                 .parallelStream()
                 .collect(Collectors.toConcurrentMap(
-                        dbBenchmarkExecutorEntry -> dbBenchmarkExecutorEntry.getKey().getSimpleName(),
-                        dbBenchmarkExecutorEntry ->
-                                new BenchmarkResult(IntStream.range(0, numberOfRepetitions == null ? defaultNumberOfRepetitions : numberOfRepetitions)
-                                        .mapToDouble(i -> dbBenchmarkExecutorEntry.getValue().executeBenchmark(query))
-                                        .average().orElse(0)
-                                )
-                ));
+                        dbConnectionEntry -> dbConnectionEntry.getKey().getSimpleName(),
+                        dbConnectionEntry -> {
+                            try {
+                                return new BenchmarkResult(
+                                        measureAverageExecutionTime(query, dbConnectionEntry.getValue(), numberOfRepetitions)
+                                );
+                            } catch (SQLException e) {
+                                throw new SQLExceptionWrapper(e, dbConnectionEntry.getKey());
+                            }
+                        })
+                );
+    }
+
+    private double measureAverageExecutionTime(String query, Connection connection, Integer numberOfRepetitions) throws SQLException {
+        numberOfRepetitions = numberOfRepetitions == null ? defaultNumberOfRepetitions : numberOfRepetitions;
+
+        double totalExecutionTime = 0;
+        int rowCountTotal = 0;
+
+        for (int i = 0; i <= numberOfRepetitions; i++) {
+            try (PreparedStatement preparedStatement =
+                         connection.prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
+                long startTime = System.nanoTime();
+
+                ResultSet rs = preparedStatement.executeQuery();
+
+                double executionTime = (System.nanoTime() - startTime) / 1_000_000.;
+
+                if(executionTime == 0) {
+                    numberOfRepetitions++;
+                }
+
+                totalExecutionTime += executionTime;
+
+                if(rs.last()) {
+                    rowCountTotal += rs.getRow();
+                }
+            }
+        }
+
+        logger.info("row count {}", rowCountTotal / numberOfRepetitions);
+
+        return totalExecutionTime / numberOfRepetitions;
     }
 }
